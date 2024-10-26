a5f3621e22f9bfe0c06b423b302a3697b4b61f08
aadam-11 <aadam.marwat@city.ac.uk>
2024-10-26 18:28:38 +0100

    Update task2.sql

--- HW 4 /task2.sql
+++ HW 4 /task2.sql
@@ -5,7 +5,7 @@ INSERT INTO petEvent (petname, eventdate, eventtype, remark)
 VALUES ('Hammy', '2020-10-15', 'vet', 'antibiotics');
 
 INSERT INTO petPet (petname, owner, species, gender, birth)
-VALUES ('Hammy', 'Diane', 'Hamster', 'M', '2010-10-30'); 
+VALUES ('Hammy', 'Diane', 'Hamster', 'M', '2010-10-30');
 
 UPDATE petPet
 SET death = '2020-09-01'
@@ -13,9 +13,6 @@ WHERE petname = 'Puffball';
 
 INSERT INTO petEvent (petname, eventdate, eventtype, remark)
 VALUES ('Claws', '1997-08-03', 'vet', 'broken rib');
-
-INSERT INTO petPet (petname, owner, species, gender, birth)
-VALUES ('Claws', 'Fluffy', 'Kitten', 'M', '2020-09-01'); 
 
 DELETE FROM petPet 
 WHERE owner = 'Harold';
